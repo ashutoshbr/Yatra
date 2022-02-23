fd0f8a67e3a4ac9b10c975771038e9e215b9afe7
abr-0 <ashutoshmandal115@gmail.com>
2022-02-23 19:21:50 +0545

    db connection

--- app/main.py
+++ app/main.py
@@ -1,6 +1,34 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import psycopg2
+from psycopg2.extras import RealDictCursor  # to show column names
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
+
+
+class Homestay(BaseModel):
+    name: str
+    description: str
+
+
+try:
+    conn = psycopg2.connect(
+        host="homestay.postgres.database.azure.com",
+        database="homestaydb",
+        user=f"{os.environ['USER']}",
+        password=f"{os.environ['PASSWORD']}",
+        cursor_factory=RealDictCursor,
+    )
+    cursor = conn.cursor()
+    print("DB connections SUCCESS")
+except Exception as error:
+    print("Connection to DB Failed")
+    print("ERROR!!!", error)
+
 
 @app.get("/")
 def home():
