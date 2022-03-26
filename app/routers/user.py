ecbe02a83c71d9402e6512193e377414c91e4bfd
abr-0 <ashutoshmandal115@gmail.com>
2022-03-26 19:52:33 +0545

    hash password

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter, HTTPException, status, Response
-from ..database import cursor
+from ..database import cursor, conn
+from .. import schemas, utils
+
 
 router = APIRouter(prefix="/user", tags=["User"])
 
@@ -9,3 +11,15 @@ def get_user():
     cursor.execute(""" SELECT * FROM userinfo """)
     users = cursor.fetchall()
     return users
+
+@router.post("/")
+def add_homestay(user: schemas.User):
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+    cursor.execute(
+        """ INSERT INTO userinfo (email, password) VALUES (%s, %s) RETURNING *""",
+        (user.email, user.password)
+    )
+    conn.commit()
+    new_user = cursor.fetchone()
+    return new_user
