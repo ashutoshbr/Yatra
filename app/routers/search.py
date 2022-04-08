6d1906293bb0dfaac21ba19cdce3524b83eeb6ac
abr-0 <ashutoshmandal115@gmail.com>
2022-04-08 16:16:47 +0545

    add location filter

--- app/routers/search.py
+++ app/routers/search.py
@@ -9,9 +9,10 @@ router = APIRouter(prefix="/search", tags=["Search"])
 
 
 @router.get("/", response_model=List[schemas.GetHomestay])
-def get_req_homestay(name: str):
+def get_req_homestay(name: str | None = None, location: str | None = None):
     cursor.execute(
-        """ SELECT * FROM homestay WHERE LOWER(name) LIKE LOWER(%s) """, (name,)
+        """ SELECT * FROM homestay WHERE LOWER(name) LIKE LOWER(%s) OR LOWER(location) LIKE(%s)""",
+        (name, location),
     )
     homestays = cursor.fetchall()
     return homestays
